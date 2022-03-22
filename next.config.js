90961566be2b4e27308b71297ad35727454508da
Seokseong Jeon <6747391+gsongsong@users.noreply.github.com>
2022-03-22 09:46:26 +0000

    add asset prefix for production

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/proj3rd.github.io/' : '',
+  assetPrefix: isProd ? 'https://cdn.jsdelivr.net/gh/proj3rd/proj3rd.github.io/docs' : '',
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
