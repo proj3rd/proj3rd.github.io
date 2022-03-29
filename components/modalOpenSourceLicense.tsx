142890f1da6443dc15483d2517fc4ac1d546727a
Seokseong Jeon <6747391+gsongsong@users.noreply.github.com>
2022-03-29 08:05:21 +0000

    update open source license

--- components/modalOpenSourceLicense.tsx
+++ components/modalOpenSourceLicense.tsx
@@ -22,9 +22,19 @@ export default function ModalOpenSourceLicense(props: Props) {
     p("grammar3rd", "MIT", "https://github.com/proj3rd/grammar3rd"),
     p("asn3rd", "MIT", "https://github.com/proj3rd/asn3rd"),
     p("ANTLR4", "BSD 3-clause, MIT", "https://www.antlr.org"),
+    p("word-extractor", "MIT", "https://www.npmjs.com/package/word-extractor"),
     p("Next.js", "MIT", "https://nextjs.org"),
+    p("webpack", "MIT", "https://webpack.js.org"),
     p("React", "MIT", "https://reactjs.org"),
     p("Ant Design", "MIT", "https://ant.design/"),
+    p("assert", "MIT", "https://www.npmjs.com/package/assert"),
+    p("buffer", "MIT", "https://www.npmjs.com/package/buffer"),
+    p("events", "MIT", "https://www.npmjs.com/package/events"),
+    p("path-browserify", "MIT", "https://www.npmjs.com/package/path-browserify"),
+    p("process", "MIT", "https://www.npmjs.com/package/process"),
+    p("stream-browserify", "MIT", "https://www.npmjs.com/package/stream-browserify"),
+    p("util", "MIT", "https://www.npmjs.com/package/util"),
+    p("browserify-zlib", "MIT", "https://www.npmjs.com/package/browserify-zlib"),
   ];
 
   return (
